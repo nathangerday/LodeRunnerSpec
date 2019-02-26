2fb02d7be1d76bbb792db6f7c28eaf892f362fcb
Nathan Gerday <nat.gerday@gmail.com>
2019-02-26 19:54:40 +0100

    Add Decorator

--- TME3/src/FilesPrioDecorator.java
+++ TME3/src/FilesPrioDecorator.java
@@ -1,8 +1,63 @@
+import java.util.Set;
 
-public class FilesPrioDecorator<T> {
-	private FilesPrio<T> filep;
+public class FilesPrioDecorator<T> implements FilesPrio<T>{
+	private FilesPrio<T> delegates;
 	
 	public FilesPrioDecorator(FilesPrio<T> f) {
-		this.filep = f;
+		this.delegates = f;
 	}
+
+	public int getSize() {
+		return delegates.getSize();
+	}
+
+	public boolean isEmpty() {
+		return delegates.isEmpty();
+	}
+
+	public Set<Integer> getActivePrios() {
+		return delegates.getActivePrios();
+	}
+
+	public boolean isActive(int i) {
+		return delegates.isActive(i);
+	}
+
+	public int getMaxPrio() {
+		return delegates.getMaxPrio();
+	}
+
+	public int getSizePrio(int i) {
+		return delegates.getSizePrio(i);
+	}
+
+	public T getPrio(int i) {
+		return delegates.getPrio(i);
+	}
+
+	public T getElem() {
+		return delegates.getElem();
+	}
+
+	public T getElemPrio(int i, int k) {
+		return delegates.getElemPrio(i, k);
+	}
+
+	public void putPrio(int i, T e) {
+		delegates.putPrio(i, e);
+	}
+
+	public void put(T e) {
+		delegates.put(e);
+	}
+
+	public void removePrio(int i) {
+		delegates.removePrio(i);
+	}
+
+	public void remove() {
+		delegates.remove();
+	}
+
+	
 }
